492e136c97e5ee009c1b43ced1e48d88a2d08ff3
dev-kudli <sudarshan97.kudli@gmail.com>
2023-10-11 15:29:29 -0400

    removed webapp files

--- deployment/start.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get update
-sudo apt-get install postgresql
-sudo apt install nodejs npm -y
-
-sudo service postgresql start
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
