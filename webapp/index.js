d1fc3e60f7ab612ac46d43b36b52f6b02540f42e
dev-kudli <sudarshan97.kudli@gmail.com>
2023-10-16 14:49:33 -0400

    ec2 using AMI working

--- webapp/index.js
+++ webapp/index.js
@@ -8,7 +8,7 @@ const project = config.require("project");
 const vpcCidrBlock = config.require("vpcCidrBlock");
 const maxAllowedAzs = config.require("maxAllowedAzs");
 
-var azs = []
+var azs = [];
 
 /**
  * Generate dynamic tags for resources.
@@ -22,7 +22,7 @@ const generateTags = (resourceName, additionalTags = []) => {
   };
   const tags = { ...baseTags, ...additionalTags };
   return tags;
-}
+};
 
 const loadAvailabilityZones = async () => {
   try {
@@ -33,7 +33,7 @@ const loadAvailabilityZones = async () => {
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 const amiId = "ami-09d8eefafe3ffd3c9";
 
@@ -42,34 +42,39 @@ const getAmi = async () => {
     const ami = await aws.ec2.getAmi({
       owners: ["self"],
       mostRecent: true,
-      filters: [{
+      filters: [
+        {
           name: "image-id",
           values: [amiId],
-      }],
+        },
+      ],
     });
     return ami;
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 (async () => {
   //Load AZs for a given region
   await loadAvailabilityZones();
   const ami = await getAmi();
-  
+
   // Create a new VPC.
-  const myVpc = new aws.ec2.Vpc(generateTags('vpc').Name, {
+  const myVpc = new aws.ec2.Vpc(generateTags("vpc").Name, {
     cidrBlock: vpcCidrBlock,
     defaultRouteTableAssociation: false,
-    tags: generateTags('vpc'),
+    tags: generateTags("vpc"),
   });
 
   // Create an Internet Gateway and attach it to the VPC.
-  const myInternetGateway = new aws.ec2.InternetGateway(generateTags('ig').Name, {
-    vpcId: myVpc.id,
-    tags: generateTags('ig'),
-  });
+  const myInternetGateway = new aws.ec2.InternetGateway(
+    generateTags("ig").Name,
+    {
+      vpcId: myVpc.id,
+      tags: generateTags("ig"),
+    },
+  );
 
   // Create a public subnets in given availability zone.
   const publicSubnets = azs.map((az, index) => {
@@ -86,91 +91,119 @@ const getAmi = async () => {
     return new aws.ec2.Subnet(generateTags(`pvt-sn-${index}`).Name, {
       vpcId: myVpc.id,
       availabilityZone: az,
-      cidrBlock: `10.0.${index+azs.length}.0/24`,
+      cidrBlock: `10.0.${index + azs.length}.0/24`,
       tags: generateTags(`pvt-sn-${index}`),
     });
   });
 
   // Create a public route table and associate it with public subnets.
-  const publicRouteTable = new aws.ec2.RouteTable(generateTags('pub-rtable').Name, {
-    vpcId: myVpc.id,
-    routes: [
-      {
-        cidrBlock: "0.0.0.0/0",
-        gatewayId: myInternetGateway.id,
-      },
-  ],
-    tags: generateTags('pub-rtable'),
-  });
+  const publicRouteTable = new aws.ec2.RouteTable(
+    generateTags("pub-rtable").Name,
+    {
+      vpcId: myVpc.id,
+      routes: [
+        {
+          cidrBlock: "0.0.0.0/0",
+          gatewayId: myInternetGateway.id,
+        },
+      ],
+      tags: generateTags("pub-rtable"),
+    },
+  );
 
   publicSubnets.forEach((subnetId, index) => {
-    const subnetRouteTableAssociation = new aws.ec2.RouteTableAssociation(`pubRouteTableAssoc-${index}`, {
-      subnetId: subnetId,
-      routeTableId: publicRouteTable.id,
-    });
+    const subnetRouteTableAssociation = new aws.ec2.RouteTableAssociation(
+      `pubRouteTableAssoc-${index}`,
+      {
+        subnetId: subnetId,
+        routeTableId: publicRouteTable.id,
+      },
+    );
   });
 
   // Create a private route table and associate it with private subnets.
-  const privateRouteTable = new aws.ec2.RouteTable(generateTags('pvt-rtable').Name, {
-    vpcId: myVpc.id,
-    tags: generateTags('pvt-rtable'),
-  });
+  const privateRouteTable = new aws.ec2.RouteTable(
+    generateTags("pvt-rtable").Name,
+    {
+      vpcId: myVpc.id,
+      tags: generateTags("pvt-rtable"),
+    },
+  );
 
   privateSubnets.forEach((subnetId, index) => {
-    const subnetRouteTableAssociation = new aws.ec2.RouteTableAssociation(`pvtRouteTableAssoc-${index}`, {
-      subnetId: subnetId,
-      routeTableId: privateRouteTable.id,
-    });
+    const subnetRouteTableAssociation = new aws.ec2.RouteTableAssociation(
+      `pvtRouteTableAssoc-${index}`,
+      {
+        subnetId: subnetId,
+        routeTableId: privateRouteTable.id,
+      },
+    );
   });
 
-  const routeTableAssociation = new aws.ec2.RouteTableAssociation("routeTableAssociation", {
+  const routeTableAssociation = new aws.ec2.RouteTableAssociation(
+    "routeTableAssociation",
+    {
       gatewayId: myInternetGateway.id,
       routeTableId: privateRouteTable.id,
+    },
+  );
+
+  const ec2SecurityGroup = new aws.ec2.SecurityGroup(
+    generateTags("ec2-sg").Name,
+    {
+      name: generateTags("ec2-sg").Name,
+      description: "Allow incoming SSH and TCP",
+      vpcId: myVpc.id,
+      ingress: [
+        {
+          protocol: "tcp",
+          fromPort: 22,
+          toPort: 22,
+          cidrBlocks: ["0.0.0.0/0"],
+        },
+        {
+          protocol: "tcp",
+          fromPort: 80,
+          toPort: 80,
+          cidrBlocks: ["0.0.0.0/0"],
+        },
+        {
+          protocol: "tcp",
+          fromPort: 8000,
+          toPort: 8000,
+          cidrBlocks: ["0.0.0.0/0"],
+        },
+      ],
+      egress: [
+        {
+          fromPort: 0,
+          toPort: 0,
+          protocol: "-1",
+          cidrBlocks: ["0.0.0.0/0"],
+        },
+      ],
+    },
+  );
+
+  const userData =
+    // <-- ADD THIS DEFINITION
+    `#!/bin/bash
+echo "Hello, World!" > index.html
+nohup python -m SimpleHTTPServer 80 &`;
+
+  const ec2Instance = new aws.ec2.Instance(generateTags("ec2"), {
+    ami: "ami-09d8eefafe3ffd3c9",
+    instanceType: "t2.micro",
+    keyName: "csye6225-dev-key",
+    subnetId: publicSubnets[0].id,
+    vpcSecurityGroupIds: [ec2SecurityGroup.id],
+    associatePublicIpAddress: true,
+    userData: userData,
   });
-
-  const ec2SecurityGroup = new aws.ec2.SecurityGroup(generateTags('ec2-sg').Name, {
-    name: generateTags('ec2-sg').Name,
-    description: "Allow incoming SSH and TCP",
-    vpcId: myVpc.id,
-    ingress: [
-        {
-            protocol: "tcp",
-            fromPort: 22,
-            toPort: 22,
-            cidrBlocks: ["0.0.0.0/0"],
-        },
-        {
-            protocol: "tcp",
-            fromPort: 80,
-            toPort: 80,
-            cidrBlocks: ["0.0.0.0/0"],
-        },
-        {
-            protocol: "tcp",
-            fromPort: 8000,
-            toPort: 8000,
-            cidrBlocks: ["0.0.0.0/0"],
-        },
-    ],
-});
-
-const ec2Instance = new aws.ec2.Instance(generateTags('ec2'), {
-  ami: "ami-09d8eefafe3ffd3c9",
-  instanceType: ami.imageLocation.split("/")[1],
-  keyName: "csye6225-key-pair",
-  networkInterfaces: [
-      {
-          associatePublicIpAddress: true,
-          deviceIndex: 0,
-          securityGroups: [ec2SecurityGroup.name],
-          subnetId: publicSubnets[0].id,
-      },
-  ],
-});
 
   // Export the VPC ID and other resources if needed.
   exports.vpcId = myVpc.id;
   exports.InternetGatewayId = myInternetGateway.id;
   exports.publicSubnetIds = publicSubnets.map((subnet) => subnet.id);
   exports.privateSubnetIds = privateSubnets.map((subnet) => subnet.id);
-})()
+})();
